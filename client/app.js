cf38cc14f38916f003d0b96952e4f3612a8f2af8
mel-am <122634403+mel-am@users.noreply.github.com>
2024-04-16 13:07:46 +0100

    update
    Co-Authored-By: Toyah <137830874+Toyah1427@users.noreply.github.com>
    Co-Authored-By: Jerrylee Davis <137108689+mack070@users.noreply.github.com>

---
+++ client/app.js
@@ -0,0 +1,28 @@
+const songWrapper = document.getElementById("songWrapper");
+
+// get the songs from my api
+async function getSongs() {
+  const response = await fetch("http://localhost:8080/songs");
+  const songs = await response.json();
+  console.log(songs);
+
+  // put the songs onto the page
+  songs.forEach(function (song) {
+    // DOM manipulation to put the songs onto the html
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+      const video = document.createElement("iframe");
+      video.setAttribute("src", song.video)
+      console.log(song)
+
+    h2.textContent = song.song_title;
+    // p.textContent = `This is ${}`;
+    // img.src = game.imgUrl;
+    // img.alt = game.title;
+    songWrapper.appendChild(h2);
+    songWrapper.appendChild(h2);
+    songWrapper.appendChild(video);
+  });
+}
+
+getSongs();
