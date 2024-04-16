23195fa77d2d9eb2fe670dedc5894962df308d0b
mel-am <122634403+mel-am@users.noreply.github.com>
2024-04-16 10:41:55 +0100

    updated seed
    Co-Authored-By: Toyah <137830874+Toyah1427@users.noreply.github.com>
    Co-Authored-By: Jerrylee Davis <137108689+mack070@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
 app.use(express.json());
+app.use(cors());
 
-// your routes will go here...
+// connect to the database.db file (normally this would be "connect to the database that is hosted elsewhere")
+import Database from "better-sqlite3";
+const db = new Database("database.db"); // go and get the existing db file
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000...");
+app.get("/", function (request, response) {
+  response.json("You are looking at my root route. How roude.");
 });
-app.get("/messages", (req, res) => {
-    res.json({ message: "Hello, World!" });
-  });
-   
+
+app.get("/games", function (request, response) {
+  // here we use .all instead of .run because we aren't INSERTing, but selecting. So we want to see ALL the results
+  const games = db.prepare("SELECT * FROM songs").all();
+  response.json(games);
+});
+
+app.listen(8080, function () {
+  console.log("HAHAHAH... 8080");
+});
