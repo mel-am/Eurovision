4f4fb26f47a20109abce1c8f65cb6a598bf07b9f
Jerrylee <jerrylee.davis@hotmail.co.uk>
2024-04-17 15:54:27 +0100

    container-update

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-const songWrapper = document.getElementById("song");
+const songWrapper = document.getElementById("songWrapper");
 
 // get the songs from my api
 async function getSongs() {
@@ -9,19 +9,23 @@ async function getSongs() {
   // put the songs onto the page
   songs.forEach(function (song) {
     // DOM manipulation to put the songs onto the html
+    const div = document.createElement("div");
     const h2 = document.createElement("h2");
     const p = document.createElement("p");
-      const video = document.createElement("iframe");
-      video.setAttribute("src", song.video)
-      console.log(song)
+    const video = document.createElement("iframe");
+    video.setAttribute("src", song.video);
+    console.log(song);
 
-    h2.textContent =h2.textContent = `${song.country}- ${ song.artist }-  ${song.song_title}`;
+    h2.textContent =
+      h2.textContent = `${song.country}- ${song.artist}-  ${song.song_title}`;
     // p.textContent = `This is ${}`;
     // img.src = game.imgUrl;
     // img.alt = game.title;
-    songWrapper.appendChild(h2);
-    songWrapper.appendChild(h2);
-    songWrapper.appendChild(video);
+    div.appendChild(h2);
+    div.appendChild(p);
+    div.appendChild(video);
+    div.classList.add("video");
+    songWrapper.appendChild(div);
   });
 }
 
@@ -31,46 +35,44 @@ const messagesList = document.getElementById("messagesList");
 
 // Get messages from the API
 async function getMessages() {
-    const response = await fetch("http://localhost:8080/messages"); // Change to Render SERVER URL before submitting
-    const messages = await response.json();
-console.log(messages)
+  const response = await fetch("http://localhost:8080/messages"); // Change to Render SERVER URL before submitting
+  const messages = await response.json();
+  console.log(messages);
 
+  // Iterate through the messages and append them to the list
+  messages.forEach(function (message) {
+    // Create elements for message display
+    const li = document.createElement("li");
+    const namePara = document.createElement("p");
+    const p = document.createElement("p");
 
-    // Iterate through the messages and append them to the list
-    messages.forEach(function (message) {
-      // Create elements for message display
-      const li = document.createElement("li");
-      const namePara = document.createElement("p"); 
-      const p = document.createElement("p");
+    // Set the text content of the paragraph to the message text
+    namePara.textContent = message.name;
+    p.textContent = message.message;
+    console.log(p.textContent);
 
-      // Set the text content of the paragraph to the message text
-      namePara.textContent = message.name; 
-      p.textContent = message.message; 
-console.log(p.textContent);
+    // Append the paragraph to the list item
+    li.appendChild(namePara);
+    li.appendChild(p);
 
-
-      // Append the paragraph to the list item
-      li.appendChild(namePara);
-      li.appendChild(p);
-      
-
-      // Append the list item to the messages list
-      messagesList.appendChild(li);
-      messagesList.appendChild(li);
-    });
-  }
+    // Append the list item to the messages list
+    messagesList.appendChild(li);
+    messagesList.appendChild(li);
+  });
+}
 
 // Event listener for form submission
-document.getElementById("messageForm").addEventListener("submit", async (event) => {
-  event.preventDefault();
+document
+  .getElementById("messageForm")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  // Get the message from the input field
-  const nameInput = document.getElementById("nameInput");
-  const yourName = event.target.name.value; // CHANGED - ADDED event.target.yourName
-  const messageInput = document.getElementById("messageInput");
-  const message = event.target.message.value; // CHANGED - ADDED event.target.message
-  console.log({nameInput, messageInput, yourName, message}) 
-
+    // Get the message from the input field
+    const nameInput = document.getElementById("nameInput");
+    const yourName = event.target.name.value; // CHANGED - ADDED event.target.yourName
+    const messageInput = document.getElementById("messageInput");
+    const message = event.target.message.value; // CHANGED - ADDED event.target.message
+    console.log({ nameInput, messageInput, yourName, message });
 
     // Send the message to the API - // replace with Render URL at the end before submission
     await fetch("http://localhost:8080/messages", {
@@ -78,9 +80,9 @@ document.getElementById("messageForm").addEventListener("submit", async (event) 
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ name: yourName, message:message}), // add more input into curly braces - DONE?
+      body: JSON.stringify({ name: yourName, message: message }), // add more input into curly braces - DONE?
     });
 
-// Fetch messages when the page loads
-getMessages();
-  })
+    // Fetch messages when the page loads
+    getMessages();
+  });
